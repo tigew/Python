b284981513ff6c3b1899645810248663d7ff8281
Travis Peebles <cheadge@gmail.com>
2017-10-23 10:07:44 -0700

    Finish quiz

--- if-elsequiz.py
+++ if-elsequiz.py
@@ -1,1 +1,11 @@
 # book says take road less traveled, which way dose she go?
+
+def determine_direction(message):
+    if message == "TAKE THE ROAD LESS TRAVELLED":
+        print "Turn Left"
+    elif message == "COMFORT IS DIVINE":
+        print "Turn Right"
+    else:
+        print "I don't know"
+       
+determine_direction("TAKE THE ROAD LESS TRAVELLED")
